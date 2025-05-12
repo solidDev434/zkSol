c4a5ffb4fdaa5463d560f4f1cecc5aacb1eb21e2
Joseph Ibok <josephibok75@gmail.com>
2025-05-12 02:34:36 +0100

    Completed Solana Pay integration

--- app/pay/page.tsx
+++ app/pay/page.tsx
@@ -1,15 +1,46 @@
 "use client";
 
-import React from 'react'
+import React, { useRef } from 'react'
 import { useWallet } from '@solana/wallet-adapter-react';
-import { PublicKey } from '@solana/web3.js';
+import { Keypair, PublicKey } from '@solana/web3.js';
 import {
-  enc
-}
+  encodeURL,
+  createQR
+} from "@solana/pay";
+import BigNumber from "bignumber.js";
 
 const Pay = () => {
+  const qrRef = useRef<HTMLDivElement>(null);
+
+  const recipient = new PublicKey("CN5iQKU2dY4SbPFYVjx4SfxJPLMPH4qRhnagkCnqPxQY");
+  const amount = new BigNumber(0.7);
+  const reference = new Keypair().publicKey;
+  const label = "ZKCompress Swap";;
+  const message = "ZKCompress Swap - #132493";
+  const memo = "ZC#2343";
+  
+  const url = encodeURL({
+    recipient,
+    amount,
+    reference,
+    label,
+    message,
+    memo
+  });
+  const qrCode = createQR(url);
+
+  if (qrRef.current) {
+    qrRef.current.innerHTML = "";
+    qrCode.append(qrRef.current);
+  }
+
+  console.log(url, qrCode);
+
   return (
-    <div>Pay</div>
+    <div>
+      Pay
+      <div ref={qrRef}></div>
+    </div>
   )
 }
 
